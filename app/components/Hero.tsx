1696a884db358b730689b1dfc03beaee87bd99bc
suhail-ibne-habib <suhailibnehabib@gmail.com>
2026-06-04 13:53:44 +0600

    fixed color of the practical ai

--- app/components/Hero.tsx
+++ app/components/Hero.tsx
@@ -22,7 +22,7 @@ export default function Hero() {
         <h1 className="font-orbitron font-extrabold text-5xl md:text-7xl lg:text-8xl tracking-tight leading-tight mb-8 text-white">
           Transforming Business<br /> Operations Through
           <br />
-          <span className="text-cyber-cyan">Practical AI</span>
+          <span className="text-cyber-cyan">Practical </span>AI
         </h1>
 
         {/* Sub-description */}
